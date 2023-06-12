bdb13fd6eed864be03a922fd8dff0dedc93e1146
Pratik Agarwal <pratikkumar.agarwal123@gmail.com>
2023-06-12 20:34:37 +0530

    changed address

--- backend/constants.js
+++ backend/constants.js
@@ -1,5 +1,5 @@
 // Replace the value with your NFT contract address
 const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
-  "0x5ccB0e601C6667bCb70b8102De69fE6B49675804";
+  "0xa8bf0f48EA49565d27a4cbFF6692254Fe98C56A2";
 
 module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
